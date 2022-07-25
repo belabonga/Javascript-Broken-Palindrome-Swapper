a04c2ba9072ace5cdd4a67c9fa1fe33ced503181
Shania <shania1faza@gmail.com>
2022-07-25 19:12:31 +0700

    upload index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+/**
+ * @function palindromeSwapper
+ * menukar 2 huruf berurutan dari n = 0 sampai n-1
+ * contoh: makan => amkan, mkaan, maakn, makna
+ * @param {string} str - input kata yang ingin di-swap dan dicek palindrom
+ * @returns {boolean} true bila kata yang di-swap adalah palindrom
+ */
+
+function palindromeSwapper(str) {
+  if (isPalindrome(str)) return true;
+  for (var i = 0; i < str.length; i++) {
+    let newStr = '';
+    for (var j = 0; j < str.length; j++) {
+      if (i === j) {
+        newStr += str[j + 1];
+        newStr += str[j];
+        j++;
+      } else {
+        newStr += str[j];
+      }
+    }
+  if (isPalindrome(newStr)) return true;
+  }
+  return false;
+}
+
+/**
+ * @function isPalindrome
+ * Cek apabila kata merupakan palindromeSwapper
+ * @param {string} str - input kata yang dicek bila palindrom
+ * @returns {boolean} true bila kata adalah palindrom
+ */
+
+function isPalindrome(str) {
+  if (str.split('').reverse().join('') === str) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+console.log(palindromeSwapper('arcecar')); // true
+console.log(palindromeSwapper('racecar')); // true
+console.log(palindromeSwapper('recacar')); // false
+console.log(palindromeSwapper('icvic')) // true
+console.log(palindromeSwapper('makan')) // false
